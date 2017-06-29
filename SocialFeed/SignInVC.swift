90ecba4df2ffbe694ab38a4cbf25e9fe5e99dd3f
tsasselli <tsasselli@gmail.com>
2017-06-29 14:55:42 -0700

    inital commit
    added firebase pods

---
+++ SocialFeed/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialFeed
+//
+//  Created by Travis Sasselli on 6/29/17.
+//  Copyright © 2017 TravisSasselli. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
